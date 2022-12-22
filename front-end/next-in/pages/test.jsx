841466df8892f8dfb148eda73126daa5a1543c6b
skshm2000 <107468465+skshm2000@users.noreply.github.com>
2022-12-22 19:28:26 +0530

    Chatroom implement test

--- front-end/next-in/pages/test.jsx
+++ front-end/next-in/pages/test.jsx
@@ -1,26 +1,10 @@
-import { useState, useEffect } from 'react'
-import io from 'socket.io-client'
-
-const endpoint = 'http://localhost:8080/'
-let socket;
+import Chat from '../components/Chat/Chat'
 
 export default function Tester() {
 
-    useEffect(()=>{
-        socket = io.connect(endpoint)
-        socket.emit('setup', '63a417eb102fa15a6c9843eb')
-        socket.emit('newMsg', 
-            {
-                msg:"Or bhai kya haal h",
-                sender:"63a3f0feba34e12291c0b54a",
-                chat:"63a417eb102fa15a6c9843eb"
-            }
-        )
-    }, [])
-
     return (
         <>
-        <h1>Hello</h1>
+        <Chat />
         </>
     )
 }
